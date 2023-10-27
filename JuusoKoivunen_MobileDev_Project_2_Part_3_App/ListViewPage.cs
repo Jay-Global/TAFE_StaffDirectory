4ac3c249258b40d60d211d833d37a2e939641ab8
Juuso Jay Koivunen <juuso.koivunen.personal@gmail.com>
2023-10-27 11:14:22 +1100

    Shell Navigation 1.0.0
    Flyout styles added, still issue with a "navbar" sizing and icons.

---
+++ JuusoKoivunen_MobileDev_Project_2_Part_3_App/ListViewPage.cs
@@ -0,0 +1,15 @@
+﻿namespace JuusoKoivunen_MobileDev_Project_2_Part_3_App;
+
+public class ListViewPage : ContentPage
+{
+	public ListViewPage()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
