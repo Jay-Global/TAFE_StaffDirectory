ce6d7a475ecb15df0e8387b45f2085058f93d6ac
Juuso Jay Koivunen <juuso.koivunen.personal@gmail.com>
2023-11-03 10:04:10 +1100

    Login View 1.1.0
    Styling done, reorganising project files

---
+++ JuusoKoivunen_MobileDev_Project_2_Part_3_App/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+
+global using JuusoKoivunen_MobileDev_Project_2_Part_3_App.ViewModels;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
+
